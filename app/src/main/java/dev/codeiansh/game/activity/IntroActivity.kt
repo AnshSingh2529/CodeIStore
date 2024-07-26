29d9020c5e7c204b3ee9dfa976167fe8c972798a
Ansh Singh <AnshSingh@2529>
2024-07-26 13:19:48 +0530

    Layout design complete

---
+++ app/src/main/java/dev/codeiansh/game/activity/IntroActivity.kt
@@ -0,0 +1,17 @@
+package dev.codeiansh.game.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dev.codeiansh.game.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_intro)
+
+    }
+}
